af3354f7365bf83914cd199b94a93dd54fd64f65
KroneCorylus <ahustinkrone@gmail.com>
2024-03-29 10:18:54 -0300

    add scrollview

---
+++ lua/custom/plugins/minimap.lua
@@ -0,0 +1,12 @@
+return {
+  "dstein64/nvim-scrollview"
+  -- "lewis6991/satellite.nvim"
+  --   "wfxr/minimap.vim",
+  --   lazy = false,
+  --   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+  --   init = function()
+  --     vim.cmd("let g:minimap_width = 10")
+  --     vim.cmd("let g:minimap_auto_start = 1")
+  --     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+  --   end,
+}
