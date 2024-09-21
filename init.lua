0075e6ffa287437449c46cd822f3100912d1b1ad
KroneCorylus <ahustinkrone@gmail.com>
2024-09-21 11:06:14 -0300

    feat: add local plugin

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
+  { dir = '/home/krone/Development/dis.nvim' },
   { import = '.plugins' },
 }, {})
 
